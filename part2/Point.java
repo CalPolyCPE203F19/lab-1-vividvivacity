ddb4bf03ee943fc55d83ae3312d1a16288cbe228
vjchow <vjchow@csc302x23.ad.calpoly.edu>
2019-09-25 14:59:56 -0700

    finished Point class, need to add tests

---
+++ part2/Point.java
@@ -0,0 +1,38 @@
+import java.lang.Math;
+
+public class Point {
+   private double x;
+   private double y;
+
+
+
+   public Point(double x, double y) {
+      this.x = x;
+      this.y = y;
+   
+   }
+
+
+   public double getX() {
+      return x;
+   }
+
+   public double getY() {
+      return y;
+   }
+
+   public double getRadius() {
+      return Math.sqrt((x * x) + (y * y));
+   }
+
+   public double getAngle() {
+      return Math.atan(y / x);
+   }
+
+   public Point rotate90() {
+      Point newPoint = new Point((-1 * y), x);
+      return newPoint;
+   }
+
+
+}
